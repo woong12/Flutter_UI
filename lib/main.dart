9e6b25806206c1bcc2f6b3e511e9b0c2a117068d
woong <koohw0109@gmail.com>
2023-05-15 00:12:20 +0900

    custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,44 +12,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('assets/bongocat.png'),
-              SizedBox(
-                width: 200,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '고양이 팝니다',
-                      style: TextStyle(
-                        fontSize: 25,
-                      ),
-                    ),
-                    Text('금호동 3가'),
-                    Text('70000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: const [
-                        Icon(
-                          Icons.favorite,
-                        ),
-                        Text(
-                          '4',
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+        body: ShopItem(),
       ),
+    );
+  }
+}
+
+class ShopItem extends StatelessWidget {
+  const ShopItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        Text('안녕'),
+        Text('안녕'),
+        Text('안녕'),
+        Text('안녕'),
+        Text('안녕'),
+        Text('안녕'),
+        Text('안녕'),
+        Text('안녕'),
+        Text('안녕'),
+        Text('안녕'),
+        Text('안녕'),
+      ],
     );
   }
 }
