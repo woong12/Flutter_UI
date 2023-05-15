8ad59c70597ce49e43f24f057a8b2b615385911a
woong <koohw0109@gmail.com>
2023-05-15 22:03:18 +0900

    Icons and Transforms

--- lib/main.dart
+++ lib/main.dart
@@ -116,6 +116,7 @@ class MyApp extends StatelessWidget {
                 height: 20,
               ),
               Container(
+                clipBehavior: Clip.hardEdge,
                 decoration: BoxDecoration(
                   color: Color(0xff1f2123),
                   borderRadius: BorderRadius.circular(
@@ -125,6 +126,7 @@ class MyApp extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.all(30),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -137,7 +139,9 @@ class MyApp extends StatelessWidget {
                               fontWeight: FontWeight.w600,
                             ),
                           ),
-                          SizedBox(height: 10),
+                          SizedBox(
+                            height: 20,
+                          ),
                           Row(
                             children: [
                               Text(
@@ -161,6 +165,17 @@ class MyApp extends StatelessWidget {
                           )
                         ],
                       ),
+                      Transform.scale(
+                        scale: 2.2,
+                        child: Transform.translate(
+                          offset: Offset(-5, 12),
+                          child: Icon(
+                            Icons.euro_rounded,
+                            color: Colors.white,
+                            size: 88,
+                          ),
+                        ),
+                      )
                     ],
                   ),
                 ),
