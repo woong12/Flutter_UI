66e2226392c2002ede7feaeac934c225710fbd25
woong <koohw0109@gmail.com>
2023-05-19 06:49:59 +0900

    album

--- spotify_clone/lib/views/album_view.dart
+++ spotify_clone/lib/views/album_view.dart
@@ -8,20 +8,79 @@ class AlbumView extends StatefulWidget {
 }
 
 class _AlbumViewState extends State<AlbumView> {
+  late ScrollController scrollController;
+  double imageSize = 0;
+  double initialSize = 240;
+  double containerHeight = 500;
+  double containerinitialHeight = 500;
+  double imageOpacity = 1;
+  @override
+  void initState() {
+    imageSize = initialSize;
+    scrollController = ScrollController()
+      ..addListener(() {
+        imageSize = initialSize - scrollController.offset;
+        if (imageSize < 0) {
+          imageSize = 0;
+        }
+        containerHeight = containerinitialHeight - scrollController.offset;
+        if (containerHeight < 0) {
+          containerHeight = 0;
+        }
+        imageOpacity = imageSize / initialSize;
+        setState(() {});
+      });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           Container(
+            height: containerHeight,
+            width: MediaQuery.of(context).size.width,
+            alignment: Alignment.center,
             color: Colors.pink,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Opacity(
+                  opacity: imageOpacity.clamp(0, 1.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(.5),
+                          offset: const Offset(0, 20),
+                          blurRadius: 32,
+                          spreadRadius: 16,
+                        ),
+                      ],
+                    ),
+                    child: Image(
+                      image: const AssetImage("assets/album4.webp"),
+                      width: imageSize,
+                      height: imageSize,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                )
+              ],
+            ),
           ),
           SafeArea(
             child: SingleChildScrollView(
+              controller: scrollController,
               physics: const BouncingScrollPhysics(),
               child: Column(
                 children: [
                   Container(
+                    clipBehavior: Clip.none,
                     width: MediaQuery.of(context).size.width,
                     decoration: BoxDecoration(
                       gradient: LinearGradient(
@@ -35,28 +94,10 @@ class _AlbumViewState extends State<AlbumView> {
                       ),
                     ),
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 40),
+                      padding: const EdgeInsets.only(top: 40),
                       child: Column(
                         children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(.5),
-                                  offset: const Offset(0, 20),
-                                  blurRadius: 32,
-                                  spreadRadius: 16,
-                                ),
-                              ],
-                            ),
-                            child: Image(
-                              image: const AssetImage("assets/album4.webp"),
-                              width: MediaQuery.of(context).size.width - 120,
-                              height: MediaQuery.of(context).size.width - 120,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          const SizedBox(height: 16),
+                          SizedBox(height: initialSize + 32),
                           Padding(
                             padding: const EdgeInsets.all(16.0),
                             child: Column(
@@ -85,6 +126,7 @@ class _AlbumViewState extends State<AlbumView> {
                                 ),
                                 const SizedBox(height: 16),
                                 Stack(
+                                  clipBehavior: Clip.none,
                                   children: [
                                     Row(
                                       children: const [
@@ -94,14 +136,38 @@ class _AlbumViewState extends State<AlbumView> {
                                       ],
                                     ),
                                     Positioned(
-                                      left: 0,
-                                      child: Container(
-                                        width: 64,
-                                        height: 64,
-                                        decoration: const BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          color: Color(0xff14d860),
-                                        ),
+                                      right: 0,
+                                      bottom: 0,
+                                      child: Stack(
+                                        alignment: Alignment.bottomRight,
+                                        children: [
+                                          Container(
+                                            width: 64,
+                                            height: 64,
+                                            alignment: Alignment.center,
+                                            decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              color: Color(0xff14d860),
+                                            ),
+                                            child: const Icon(
+                                              Icons.play_arrow,
+                                              size: 38,
+                                            ),
+                                          ),
+                                          Container(
+                                            width: 24,
+                                            height: 24,
+                                            decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              color: Colors.white,
+                                            ),
+                                            child: const Icon(
+                                              Icons.shuffle,
+                                              color: Colors.black,
+                                              size: 14,
+                                            ),
+                                          ),
+                                        ],
                                       ),
                                     )
                                   ],
@@ -115,12 +181,34 @@ class _AlbumViewState extends State<AlbumView> {
                   ),
                   Container(
                     color: Colors.black,
-                    height: 500,
+                    height: 1000,
                   )
                 ],
               ),
             ),
-          )
+          ),
+          // App bar
+          Positioned(
+              child: Container(
+            child: Container(
+              color: const Color(0xffc61855),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
+              ),
+              child: SafeArea(
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.keyboard_arrow_left),
+                    ),
+                    const Text("Life goes on"),
+                  ],
+                ),
+              ),
+            ),
+          ))
         ],
       ),
     );
